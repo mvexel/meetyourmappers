f84eda72ed2ceca16070154852ec62b8498bbb6c
Martijn van Exel <m@rtijn.org>
2018-07-09 15:49:44 -0600

    improve run script

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-source venv/bin/activate && export FLASK_APP=meetyourmappers && export FLASK_DEBUG=true && flask run
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+export FLASK_APP=meetyourmappers
+export FLASK_DEBUG=true
+flask run
